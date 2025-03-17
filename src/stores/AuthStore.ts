deb7c6c68bc71c7c01cf65af98fd8b474c9804ef
Emerson Andrey <emersonandrey_7@hotmail.com>
2025-03-16 23:07:05 -0300

    fix: signIn

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -18,26 +18,27 @@ export const useAuthStore = defineStore('auth', () => {
   const token = ref(localStorage.getItem('token') || '');
 
   const signIn = async (userData: UserData): Promise<Partial<SignInResult>> => {
-    await axios.post('http://localhost:8080/auth/login', userData)
-      .then(function(response) {
-        const data = response.data;
-        if (data && data.token) {
-          token.value = data.token;
-          localStorage.setItem('token', data.token);
-        }
-        return {
-          successMessage: 'Successfully logged in',
-        }; 
-      })
-      .catch(function(error) {
-        const data = error.response.data;
-        const errorMessage = data.message;
-        const errorContent = data.erros?.username || data.erros?.password;
-        return {
-          errorMessage: `${errorMessage}: ${errorContent}`
-        };
-      });
-    return { errorMessage: 'Invalid credentials' };
+    try {
+      const response = await axios.post('http://localhost:8080/auth/login', userData);
+
+      console.log(response.data);
+
+      const data = response.data;
+      if (data && data.token) {
+        token.value = data.token;
+        localStorage.setItem('token', data.token);
+      }
+      return {
+        successMessage: 'Successfully logged in',
+      }; 
+    } catch (error: any) {
+      const data = error.response.data;
+      const errorMessage = data.message;
+      const errorContent = data.erros?.username || data.erros?.password || '';
+      return {
+        errorMessage: `${errorMessage} ${errorContent}`
+      };
+    }
   };
 
   const signOut = () => {
